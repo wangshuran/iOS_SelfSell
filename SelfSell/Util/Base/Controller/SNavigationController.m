738792bf6300910a4fea393e81fe046fd213883a
leeqiang250 <leeqiang250@163.com>
2018-01-22 16:01:24 +0800

    f

--- SelfSell/Util/Base/Controller/SNavigationController.m
+++ SelfSell/Util/Base/Controller/SNavigationController.m
@@ -8,24 +8,37 @@
 
 #import "SNavigationController.h"
 
-@interface SNavigationController ()
+@interface SNavigationController ()<UIGestureRecognizerDelegate>
 
 @end
 
 @implementation SNavigationController
 
+#pragma mark - Interface
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.interactivePopGestureRecognizer.delegate = self;
+}
+
+#pragma mark - UIGestureRecognizerDelegate
+
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    return self.viewControllers.count > 1;
+}
+
 #pragma mark - UINavigationBarDelegate
 
-- (BOOL)navigationBar:(UINavigationBar *)navigationBar shouldPushItem:(UINavigationItem *)item {
-    UIBarButtonItem * back = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
-    item.backBarButtonItem = back;
-    
-    UIImage *image = [[UIImage imageNamed:@"qqzoneShare"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    navigationBar.backIndicatorImage = image;
-    navigationBar.backIndicatorTransitionMaskImage = image;
-    
-    return YES;
-}
+//- (BOOL)navigationBar:(UINavigationBar *)navigationBar shouldPushItem:(UINavigationItem *)item {
+//    UIBarButtonItem * back = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
+//    item.backBarButtonItem = back;
+//
+//    UIImage *image = [[UIImage imageNamed:@"qqzoneShare"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+//    navigationBar.backIndicatorImage = image;
+//    navigationBar.backIndicatorTransitionMaskImage = image;
+//
+//    return YES;
+//}
 
 //- (void)navigationBar:(UINavigationBar *)navigationBar didPushItem:(UINavigationItem *)item {
 //}
@@ -35,5 +48,6 @@
 //
 //- (void)navigationBar:(UINavigationBar *)navigationBar didPopItem:(UINavigationItem *)item {
 //}
+
 
 @end
