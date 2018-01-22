fca35bcad76b3e20f2ce9c43b0933da37745f26a
leeqiang250 <leeqiang250@163.com>
2018-01-22 11:58:42 +0800

    d

--- SelfSell/Util/Base/Controller/SNavigationController.m
+++ SelfSell/Util/Base/Controller/SNavigationController.m
@@ -14,13 +14,26 @@
 
 @implementation SNavigationController
 
-#pragma mark - Interface
+#pragma mark - UINavigationBarDelegate
 
 - (BOOL)navigationBar:(UINavigationBar *)navigationBar shouldPushItem:(UINavigationItem *)item {
     UIBarButtonItem * back = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
     item.backBarButtonItem = back;
     
+    UIImage *image = [[UIImage imageNamed:@"qqzoneShare"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    navigationBar.backIndicatorImage = image;
+    navigationBar.backIndicatorTransitionMaskImage = image;
+    
     return YES;
 }
+
+//- (void)navigationBar:(UINavigationBar *)navigationBar didPushItem:(UINavigationItem *)item {
+//}
+//
+//- (BOOL)navigationBar:(UINavigationBar *)navigationBar shouldPopItem:(UINavigationItem *)item {
+//}
+//
+//- (void)navigationBar:(UINavigationBar *)navigationBar didPopItem:(UINavigationItem *)item {
+//}
 
 @end
